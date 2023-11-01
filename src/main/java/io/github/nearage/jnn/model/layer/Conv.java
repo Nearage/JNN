4193409e528fe4da82c0f13587e8a9fc164f46ab
vulgr <vulgr@Niobium>
2023-11-01 03:23:43 +0100

    1.0 Release Commit.

---
+++ src/main/java/io/github/nearage/jnn/model/layer/Conv.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2023 Nearage <https://github.com/Nearage>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.nearage.jnn.model.layer;
+
+import io.github.nearage.jnn.input.Matrix;
+import io.github.nearage.jnn.processing.Layer;
+
+/**
+ * Convolutional layer
+ * 
+ * @author Nearage <https://github.com/Nearage>
+ */
+public class Conv extends Layer {
+
+    @Override
+    public Matrix activate(Matrix input) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Matrix propagate(Matrix input) throws Exception {
+        return null;
+    }
+    
+}
